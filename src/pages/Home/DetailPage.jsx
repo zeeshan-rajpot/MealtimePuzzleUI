23cee1c8ffafc769c7d4103eb68089c81784a460
Gulshair <gulshairawan635@gmail.com>
2024-10-05 10:45:10 -0700

    New commit

--- src/pages/Home/DetailPage.jsx
+++ src/pages/Home/DetailPage.jsx
@@ -1,10 +1,7 @@
-import React, { useEffect, useState ,useRef} from "react";
-
-import { useReactToPrint } from "react-to-print";
+import React, { useEffect, useState, useRef } from "react";
 import Header from "../../components/Header";
 import SideBar from "../../components/SideBar";
 import { useNavigate, useParams } from "react-router-dom";
-import "./print.css";
 
 const DetailPage = () => {
   const { urn, session } = useParams();
@@ -15,19 +12,6 @@ const DetailPage = () => {
     navigate(-1);
   };
 
-  const printRef = useRef();
-
-  // Function to handle printing
-  const handlePrint = () => {
-    const printableContent = printRef.current.innerHTML; // Get the HTML content of the div
-    const originalContent = document.body.innerHTML;     // Store original body content
-
-    document.body.innerHTML = printableContent;          // Replace body with the printable div
-    window.print();                                      // Trigger the print
-    document.body.innerHTML = originalContent;           // Restore original body content
-    // window.location.reload();                            // Reload to restore event listeners
-  };
-
   const [interventionData, setInterventionData] = useState(null);
   const [loading, setLoading] = useState(true); // Loading state for the API call
 
@@ -36,13 +20,16 @@ const DetailPage = () => {
     const fetchInterventionData = async () => {
       try {
         const token = localStorage.getItem("token"); // Retrieve the token from local storage
-        const response = await fetch(`http://localhost:5001/api/get/Intervention/${urn}/${session}`, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Bearer ${token}`, // Add the token to the Authorization header
-            'Content-Type': 'application/json', // Specify the content type if necessary
-          },
-        });
+        const response = await fetch(
+          `http://localhost:5001/api/get/Intervention/${urn}/${session}`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${token}`, // Add the token to the Authorization header
+              "Content-Type": "application/json", // Specify the content type if necessary
+            },
+          }
+        );
 
         const data = await response.json();
         console.log(data);
@@ -56,13 +43,11 @@ const DetailPage = () => {
 
     fetchInterventionData();
 
-
     const intervalId = setInterval(fetchInterventionData, 2000);
 
     // Cleanup the interval on component unmount
-    return () => clearInterval(intervalId)
+    return () => clearInterval(intervalId);
   }, [urn, session]);
-
 
   const displayDataOrFallback = (field) => {
     return childInfo && childInfo[field] ? childInfo[field] : "not found";
@@ -92,7 +77,6 @@ const DetailPage = () => {
   };
 
   const sortedData = displaySortedData();
- 
 
   const handleSave = () => {
     const token = localStorage.getItem("token");
@@ -103,7 +87,8 @@ const DetailPage = () => {
     navigate("/home");
   };
 
-  console.log(interventionData)
+  // console.log(interventionData);
+
   return (
     <>
       <Header />
@@ -115,7 +100,7 @@ const DetailPage = () => {
             <button className="text-base">Back</button>
           </div>
 
-          <div ref={printRef} id="printableContent" className="w-full max-w-3xl mx-auto pb-12">
+          <div className="w-full max-w-3xl mx-auto pb-12">
             <div className="flex justify-between mb-6">
               <h2 className="text-2xl font-semibold ">Child Information</h2>
               <h2 className="text-2xl font-semibold ">
@@ -127,49 +112,68 @@ const DetailPage = () => {
               <p>Loading intervention data...</p>
             ) : (
               <div className="mt-4 space-y-6">
-                <p className="text-sm font-medium">Child's URN(Unit Record Number)</p>
+                <p className="text-sm font-medium">
+                  Child's URN(Unit Record Number)
+                </p>
                 <p className="border-b-2">{urn}</p>
                 <p className="text-sm font-medium ">Child First Name:</p>
-                <p className="border-b-2 py-1">{interventionData?.child?.firstName || "firstname"}</p>
+                <p className="border-b-2 py-1">
+                  {interventionData?.child?.firstName || "firstname"}
+                </p>
                 <p className="text-sm font-medium">Child Last Name:</p>
-                <p className="border-b-2 ">{interventionData?.child?.lastName || "not found"}</p>
+                <p className="border-b-2 ">
+                  {interventionData?.child?.lastName || "not found"}
+                </p>
                 <p className="text-sm font-medium">Parent/Caretaker Name:</p>
-                <p className="border-b-2 ">{interventionData?.child?.parentName || "not found"}</p>
+                <p className="border-b-2 ">
+                  {interventionData?.child?.parentName || "not found"}
+                </p>
                 <p className="text-sm font-medium">Date of Birth:</p>
-                <p className="border-b-2 ">{interventionData?.child?.dateOfBirth || "not found"}</p>
+                <p className="border-b-2 ">
+                  {interventionData?.child?.dateOfBirth || "not found"}
+                </p>
 
                 <p className="text-sm font-medium">Email:</p>
-                <p className="border-b-2 ">{interventionData?.child?.contactEmail || "not found"}</p>
+                <p className="border-b-2 ">
+                  {interventionData?.child?.contactEmail || "not found"}
+                </p>
                 <p className="text-sm font-medium">Contact Number:</p>
-                <p className="border-b-2 ">{interventionData?.child?.contactPhone || "not found"}</p>
+                <p className="border-b-2 ">
+                  {interventionData?.child?.contactPhone || "not found"}
+                </p>
               </div>
             )}
 
             <div className="mt-10">
               <h2 className="text-2xl font-semibold ">Mealtime</h2>
 
-              {loading ? null : (
-                interventionData?.sessionEntries?.map((step, index) => (
-                  <div key={index} className="space-y-6">
-                    <h3 className="mt-8 text-lg font-semibold">{step.domainname}</h3>
-                    <p className="text-sm font-medium">Clinical Prompt: </p>
-                    <p className="border-b-2 ">{step.clinicalPrompt || "not found"}</p>  
+              {loading
+                ? null
+                : interventionData?.sessionEntries?.map((step, index) => (
+                    <div key={index} className="space-y-6">
+                      <h3 className="mt-8 text-lg font-semibold">
+                        {step.domainname}
+                      </h3>
+                      <p className="text-sm font-medium">Clinical Prompt: </p>
+                      <p className="border-b-2 ">
+                        {step.clinicalPrompt || "not found"}
+                      </p>
                       <p className="text-sm font-medium">Formulation : </p>
-                    <p className="border-b-2 ">{step.formulation || "not found"}</p>
-                    <p className="text-sm font-medium">Recommendation: </p>
-                    <p className="border-b-2 ">{step.recommendation || "not found"}</p>
-                  </div>
-                ))
-              )}
+                      <p className="border-b-2 ">
+                        {step.formulation || "not found"}
+                      </p>
+                      <p className="text-sm font-medium">Recommendation: </p>
+                      <p className="border-b-2 ">
+                        {step.recommendation || "not found"}
+                      </p>
+                    </div>
+                  ))}
             </div>
 
             <div className="mt-10 flex justify-center space-x-4">
-              <button
-              onClick={handlePrint}
-                className="border-2 border-primary text-primary px-28 py-2 rounded-full flex"
-              >
+              <button className="border-2 border-primary text-primary px-28 py-2 rounded-full flex">
                 <img src="/lets-icons_print-duotone.svg" alt="print" />
-                Print
+                Download PDF
               </button>
             </div>
           </div>
